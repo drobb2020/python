7b42af382942f396163b26cd49d2c8207a8fa57d
drobb2020 <drobb2011@gmail.com>
2020-08-18 10:47:52 -0400

    @dependabot rebase

---
+++ crash_course/favorite_number.py
@@ -0,0 +1,11 @@
+# Python Crash Course 2e
+# Chapter 10 - Files and Exceptions
+# favorite_number.py
+# Exercise 10-11 Favorite Number:
+# Write a program that prompts for the user’s favorite number. 
+# Use json.dump() to store this number in a file. Write a separate 
+# program that reads in this value and prints the message, 
+# “I know your favorite number! It’s _____.”
+
+import json
+
